62c80023551f96a29894492abde790c470faf952
oleksandr <alexkostrytsia@gmail.com>
2024-11-20 12:53:14 +0200

    [feature] : class that work with bookings date range with method that return min, max dates;

---
+++ Tesk1BookingHalls.App/BookingDateRange.cs
@@ -0,0 +1,11 @@
+﻿namespace Tesk1BookingHalls.App;
+
+public class BookingDateRange
+{
+    public (DateTime Min, DateTime Max) Range()
+    {
+        var min = DateTime.Now;
+        var max = min.AddDays(329);
+        return (min, max);
+    }
+}
