f9e74b5f1f3aa5e2bde28eba00b531983252fdcb
oleksandr <alexkostrytsia@gmail.com>
2024-11-20 12:53:51 +0200

    [feature] : example of work;

---
+++ Tesk1BookingHalls.App/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Tesk1BookingHalls.App;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dateRange = new BookingDateRange();
+
+        var range = dateRange.Range();
+
+        Console.WriteLine($"Min: {range.Min}, Max: {range.Max}");
+    }
+}
